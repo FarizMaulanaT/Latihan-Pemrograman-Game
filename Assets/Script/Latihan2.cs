6f86324b55124451a3859d10d5fcc52095a17862
Fariz Maulana Tamim <88298399+FarizMaulanaT@users.noreply.github.com>
2021-08-20 15:08:32 +0700

    Matkul Programming
    Latihan 1-6

---
+++ Assets/Script/Latihan2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan2 : MonoBehaviour
+{
+    public int nilaiA;
+    public int nilaiB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int totall = nilaiA + nilaiB;
+        Debug.Log("Total " + totall);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
